5aed8687da38066f198752b727e718844f3a8428
danielmartin0126 <danielmartin0126@gmail.com>
2019-05-28 14:14:35 -0400

    added search filter. bones of navbar added. empty links

--- hasketball/src/components/Navbar.js
+++ hasketball/src/components/Navbar.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 
+class Navbar extends React.Component {
 
 
 
 
 
+
+
+    render(){
+       return( 
+        <div className="ui vertically divided grid">
+            <div className="one column row navbar" id="navbar">
+            <h5>Welcome playername</h5>
+            <h1>Hashketball</h1>
+            <a href="#">Login</a>
+            <a href="#">View Team</a>
+            </div>
+            <div className="one column row purple" id="searchbar">
+                <input onChange={this.props.handleFilter}></input>
+            </div>
+        </div>
+
+
+       )
+    }
+
+
+
+
+}
 export default Navbar;
--- hasketball/src/components/PlayersContainer.js
+++ hasketball/src/components/PlayersContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PlayerCard from './PlayerCard';
+import '../App.css';
 import { directive } from '@babel/types';
 
 
@@ -41,15 +42,22 @@ class PlayersContainer extends React.Component {
 		}
 
 
+		fullName = (p) => {
+			return (p.f_name + " " + p.l_name)
+		}
+
+
+	
+
+
     render(){
         return(
 					<React.Fragment>
 						<button onClick={this.backPage}>Back Page</button>
 						<button onClick={this.nextPage}>Next Page</button>
 						<div className="ui cards">
-		            {this.state.players.slice(this.state.start,this.state.end).map(player => {
-		               return <PlayerCard player={player}/>
-		            })}
+								{this.state.players.filter(p => this.fullName(p).toLowerCase().indexOf(this.props.filtered.toLowerCase()) !== -1).map(player => {
+		               return <PlayerCard player={player}/>})}
 						</div>
 
 			</React.Fragment>
