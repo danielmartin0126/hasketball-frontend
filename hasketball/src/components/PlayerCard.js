6ccc5f48665fb97ca1602ae7765448c11a1d4e3c
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-05-28 14:13:26 -0400

    added modals for all players

--- hasketball/src/components/PlayerCard.js
+++ hasketball/src/components/PlayerCard.js
@@ -1,40 +1,36 @@
 import React from 'react';
+import ShowModal from './ShowModal'
 
 
-const PlayerCard = (props) => {
+class PlayerCard extends React.Component {
 
+	fullName = () => {
+		return `${this.props.player.f_name} ${this.props.player.l_name}`
+	}
 
-return(
-	<div className="card">
-    <div className="content">
-      <div className="header">
-        {props.player.f_name +" "+ props.player.l_name}
-      </div>
-      <div className="meta">
-        Postion: {props.player.position}
-      </div>
-      <div className="description">
-        Stuff goes here
-      </div>
-    </div>
-    <div className="extra content">
-      <div className="ui two buttons">
-        <div className="ui basic green button">Draft</div>
-        <div className="ui basic blue button">View Stats</div>
-      </div>
-    </div>
-  </div>
-)
-
+	render(){
+		return(
+			<div className="card">
+		    <div className="content">
+		      <div className="header">
+		        {this.fullName()}
+		      </div>
+		      <div className="meta">
+		        Postion: {this.props.player.position}
+		      </div>
+		      <div className="description">
+		        Stuff goes here
+		      </div>
+		    </div>
+		    <div className="extra content">
+		      <div className="ui two buttons">
+		        <div className="ui basic green button">Draft</div>
+		        <ShowModal id={this.props.player.api_id} player={this.props.player} />
+		      </div>
+		    </div>
+		  </div>
+		)
+	}
 }
-
-
-
-
-
-
-
-
-
 
 export default PlayerCard;
