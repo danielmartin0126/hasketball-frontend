0bc68334d197229a00ae4ad4f5a508f923ef49c8
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-05-29 13:12:24 -0400

    added more stats

--- hasketball/src/components/ShowModal.js
+++ hasketball/src/components/ShowModal.js
@@ -21,6 +21,10 @@ class ShowModal extends React.Component {
 
 
   render(){
+<<<<<<< Updated upstream
+=======
+    console.log(this.state.playerStats)
+>>>>>>> Stashed changes
     return(
       <Modal trigger={<div id={this.props.player.api_id} onClick={this.showStats} className="ui basic blue button">View Stats</div>}>
         <Modal.Header>{this.props.player.f_name +" "+ this.props.player.l_name}</Modal.Header>
@@ -28,11 +32,13 @@ class ShowModal extends React.Component {
           <Image wrapped size='medium' src={teamLogos[this.props.player.team_name]} />
           <Modal.Description>
             <Header>2018 Season Averages</Header>
+            <p>{this.state.playerStats ? "Total Games Played: " + this.state.playerStats.games_played : null}</p>
             <ul>
               <p>{this.state.playerStats ? "Points: " + this.state.playerStats.pts : null}</p>
               <p>{this.state.playerStats ? "Assists: " + this.state.playerStats.ast : null}</p>
-              <p>{this.state.playerStats ? "Games Played: " + this.state.playerStats.games_played : null}</p>
+              <p>{this.state.playerStats ? "Turnovers: " + this.state.playerStats.turnover : null}</p>
               <p>{this.state.playerStats ? "Minutes Played: " + this.state.playerStats.min : null}</p>
+              <p>{this.state.playerStats ? "Rebounds: " + this.state.playerStats.reb : null}</p>
 
             </ul>
 
